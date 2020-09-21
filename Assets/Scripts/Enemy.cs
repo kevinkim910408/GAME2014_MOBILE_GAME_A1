72e6612d837fbe938cd1ec2760d6f886da9f7281
kevinkim0408 <kevinkim0408@naver.com>
2020-09-21 20:03:14 +0900

    Enemy Work
    Enemy movements, hit, Images, collision, destory etc.
     No attack function yet

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float moveSpeed;
+    public int hp;
+
+    SpriteRenderer spriteRenderer;
+    Rigidbody2D rigid;
+
+    [SerializeField]
+    Sprite[] sprites;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        rigid = GetComponent<Rigidbody2D>();
+        rigid.velocity = Vector2.down * moveSpeed;
+    }
+
+    void OnHit(int damage)
+    {
+        hp -= damage;
+        spriteRenderer.sprite = sprites[1];
+
+        Invoke("ReturnSprite",0.1f);
+
+        if(hp <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void ReturnSprite()
+    {
+        spriteRenderer.sprite = sprites[0];
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "BulletBorder")
+        {
+            Destroy(gameObject);
+        }
+        else if (collision.gameObject.tag == "PlayerBullet")
+        {
+            PlayerBullet playerbullet = collision.gameObject.GetComponent<PlayerBullet>();
+            OnHit(playerbullet.damage);
+        }
+    }
+
+}
--- Assets/Scripts/PlayerBullet.cs
+++ Assets/Scripts/PlayerBullet.cs
@@ -4,17 +4,7 @@ using UnityEngine;
 
 public class PlayerBullet : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public int damage;
 
      void OnTriggerEnter2D(Collider2D collision)
     {
