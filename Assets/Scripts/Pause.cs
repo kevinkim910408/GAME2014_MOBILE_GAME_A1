115793344c933bd503fdca96c06bc0b2412a2eeb
kevinkim0408 <kevinkim0408@naver.com>
2020-09-23 23:55:57 +0900

    Pause Work
    1.Pause Button in the game scene.
    2. Functions
     - Resume
     - Mainmenu

---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pause : MonoBehaviour
+{
+    [SerializeField]
+    GameObject PausePanel;
+
+    bool isPause = false;
+
+    void Awake()
+    {
+        PausePanel.gameObject.SetActive(false);
+    }
+
+    public void PauseButton()
+    {
+        if (!isPause)
+        {
+            Time.timeScale = 0;
+            isPause = true;
+        }
+        PausePanel.gameObject.SetActive(true);
+
+    }
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("Scenes/TitleScene");
+        if (isPause)
+        {
+            Time.timeScale = 1;
+            isPause = false;
+        }
+    }
+    public void Resume()
+    {
+        if (isPause)
+        {
+            Time.timeScale = 1;
+            isPause = false;
+        }
+        PausePanel.gameObject.SetActive(false);
+    }
+    public void Setting()
+    {
+        
+    }
+}
